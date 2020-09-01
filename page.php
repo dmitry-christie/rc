5f2a35dd45318fc75396d1a2b25f1f7423687d6f
Dmitry Christie <dmitry.christie@gmail.com>
2020-09-01 15:22:56 +0200

    test

--- page.php
+++ page.php
@@ -59,7 +59,7 @@ get_header();
 	</div>
 			<?php if ( is_front_page())   {?>
 				<div class="text">
-				<?php $home_text_header = get_field("home-text-header", 'option'); var_dump($home_text_header); ?>
+				<?php $home_text_header = get_field("home-text-header", 'option'); var_dump($home_text_header); ?>lorem ipsum
 			</div>
 				
 			<?php } else {?>
