f88e52845f400113564a2bf2571fd029d1c91b14
Dmitry Christie <dmitry.christie@gmail.com>
2020-09-01 15:26:24 +0200

    updated text

--- page.php
+++ page.php
@@ -59,7 +59,7 @@ get_header();
 	</div>
 			<?php if ( is_front_page())   {?>
 				<div class="text">
-				<?php the_field("home-text-header", 'option'); ?>
+				Solid decisions in everything we do.
 			</div>
 				
 			<?php } else {?>
