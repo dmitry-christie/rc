eb8bf86e1a4756e5c6b41054095b2bf48b18f104
Dmitry Christie <dmitry.christie@gmail.com>
2020-10-08 15:40:46 +0200

    added functions

--- footer.php
+++ footer.php
@@ -108,6 +108,15 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script>
 
+function SetCookie(c_name,value,expiredays)
+	{
+		var exdate=new Date()
+		exdate.setDate(exdate.getDate()+expiredays)
+		document.cookie=c_name+ "=" +escape(value)+
+		((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+	}
+</script>
 </body>
 </html>
